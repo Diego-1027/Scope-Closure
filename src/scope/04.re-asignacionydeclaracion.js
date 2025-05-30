87c0977f3e93ccd7e8922862123c1aa9494de396
Diego Rodriguez <diego.anroes1027@gmail.com>
2025-05-30 12:59:51 -0500

    Re asiganacion o re declaracion de variables

---
+++ src/scope/04.re-asignacionydeclaracion.js
@@ -0,0 +1,18 @@
+var numero1 = 1
+let numero2 = 2
+const NUMERO3 = 3
+
+var numero1 = 4 //var nos deja reasignar y redeclarar
+numero2 = 5 //let nos deja solo reasignar
+NUMERO3 // es una constante no nos deja ni reasignar y redeclarar
+
+const vehiculoas = []
+vehiculoas.push("audi")
+console.log(vehiculoas)
+
+vehiculoas.pop()
+console.log(vehiculoas)
+
+// los arrglos y los objetos funcionan algo particular ya que yo no estoy redeclarando ni reasignando como lo entiendo
+//es como tener una caja la declaro como la caja de checheres y le asigno un lugar pero gracias a los metodos
+//yo puedo añadir o sacar cosas de este caja
