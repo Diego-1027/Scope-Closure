50435b5e4c2c80742dc5d98b6bfddd925ffe1846
Diego Rodriguez <diego.anroes1027@gmail.com>
2025-05-30 13:13:09 -0500

    modulo strict

---
+++ src/scope/05.strict.js
@@ -0,0 +1,8 @@
+'use strict'
+//var pi; 
+pi = 3.141516
+console.log(pi)
+
+//En el caso de usar el use strict el sistema no me dejara pasar hasta que no declare las variables
+//Con esto hace mas stricto JS no se pone obligatorio debido a que inicialmente no era necesario para no dañar
+//las paginas mas antiguas
